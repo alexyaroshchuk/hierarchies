636c5f5fc7a37d4baff036429e2b7ed768895906
Andrei Karagayr <andreikara404@gmail.com>
2019-06-17 18:22:42 +0300

    add title

--- resources/views/criteries/priority.blade.php
+++ resources/views/criteries/priority.blade.php
@@ -14,6 +14,7 @@
                         @csrf
 
                         <div class="card-body">
+                            <p class="subtitle subtitle_black py-1 mb-3">Заголовок матрицы</p>
                             <table id="example" class="display js-numeric" style="width:100%">
                                 <thead class="table__header">
                                 <tr>
@@ -40,6 +41,7 @@
                         @if(count($criteria2) != 0)
                             @foreach($criteria1 as $cr1)
                             <div class="card-body">
+                                <p class="subtitle subtitle_black py-1 mb-3">Заголовок матрицы</p>
                                 <table id="example" class="display js-numeric" style="width:100%">
                                     <thead class="table__header">
                                     <tr>
@@ -65,6 +67,7 @@
                         @else
                             @foreach($criteria1 as $cr1)
                                 <div class="card-body">
+                                    <p class="subtitle subtitle_black py-1 mb-3">Заголовок матрицы</p>
                                     <table id="example" class="display js-numeric" style="width:100%">
                                         <thead class="table__header">
                                         <tr>
@@ -93,6 +96,7 @@
                         @if(count($criteria3) != 0  && count($criteria2) != 0)
                             @foreach($criteria2 as $cr2)
                             <div class="card-body">
+                                <p class="subtitle subtitle_black py-1 mb-3">Заголовок матрицы</p>
                                 <table id="example" class="display js-numeric" style="width:100%">
                                     <thead class="table__header">
                                     <tr>
@@ -119,6 +123,7 @@
                         @elseif(count($criteria3) == 0 && count($criteria2) != 0)
                             @foreach($criteria2 as $cr2)
                                 <div class="card-body">
+                                    <p class="subtitle subtitle_black py-1 mb-3">Заголовок матрицы</p>
                                     <table id="example" class="display js-numeric" style="width:100%">
                                         <thead class="table__header">
                                         <tr>
@@ -147,6 +152,7 @@
                         @if(count($criteria4) != 0 && count($criteria3) != 0)
                             @foreach($criteria3 as $cr3)
                                 <div class="card-body">
+                                    <p class="subtitle subtitle_black py-1 mb-3">Заголовок матрицы</p>
                                     <table id="example" class="display js-numeric" style="width:100%">
                                         <thead class="table__header">
                                         <tr>
@@ -173,6 +179,7 @@
                         @elseif(count($criteria4) == 0 && count($criteria3) != 0)
                             @foreach($criteria3 as $cr3)
                                 <div class="card-body">
+                                    <p class="subtitle subtitle_black py-1 mb-3">Заголовок матрицы</p>
                                     <table id="example" class="display js-numeric" style="width:100%">
                                         <thead class="table__header">
                                         <tr>
@@ -202,43 +209,6 @@
                             <button type="submit" class="btn-custom btn-custom_yellow" role="button" aria-pressed="true">Далее</button>
                         </div>
                     </form>
-
-
-
-                    <form id="createForm"
-                          class="form-horizontal form-label-left"
-                          method="POST"
-                          action="{{route('criteria-calculate', $hier_id)}}">
-                        @csrf
-                        <div class="card-body">
-                            <table id="example" class="display js-numeric" style="width:100%">
-                                <thead class="table__header">
-                                <tr>
-                                    {{--                                    {{dd($criteria)}}--}}
-                                    <th class="px-4">#</th>
-                                    @foreach($criteria as $cr)
-                                        <td class="table__header_text py-3 px-2">{{$cr['criteria_name']}}</td>
-                                    @endforeach
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @for($i=0; $i<count($criteria); $i++)
-                                    <tr>
-                                        <td class="table__header_text px-2">{{$criteria[$i]['criteria_name']}}</td>
-                                        @for($j=0; $j<count($criteria); $j++)
-                                            <td><input class="input__custom" type="text"  id="row-{{$i}}-{{$j}}" name="{{$criteria[$i]['id'] . "|" . $criteria[$j]['id']}}"
-                                                ></td>
-                                        @endfor
-                                    </tr>
-                                @endfor
-                                </tbody>
-                            </table>
-                        </div>
-                        <div class="card-footer">
-                            <button type="submit" class="btn-custom btn-custom_yellow" role="button" aria-pressed="true">Далее</button>
-                        </div>
-                    </form>
-
 
 
                 </div>
